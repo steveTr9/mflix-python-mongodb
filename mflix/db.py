db6693fa27731a9a3991fe4d9e12230e0d567651
SteveTr9 <nghia.trinh.1997@gmail.com>
2021-06-21 20:27:32 +0100

    Most active commenters

--- mflix/db.py
+++ mflix/db.py
@@ -519,9 +519,11 @@ def most_active_commenters():
 
     No field projection necessary.
     """
-    # TODO: User Report
-    # Return the 20 users who have commented the most on MFlix.
-    pipeline = []
+    pipeline = [
+        {"$group": {"_id": "$email", "count": {"$sum": 1}}},
+        {"$sort": {"count": DESCENDING}},
+        {"$limit": 20},
+    ]
 
     rc = db.comments.read_concern  # you may want to change this read concern!
     comments = db.comments.with_options(read_concern=rc)
